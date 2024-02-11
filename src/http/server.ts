a3aa0532ef57913b6be16bc9f0a82fe78330531a
Gustavo Benjamin <gustasbenjamin@gmail.com>
2024-02-11 19:43:08 -0300

    Finalização do desafio NodeJs

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,18 +3,23 @@ import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
 import { voteOnPoll } from "./routes/vote-on-poll";
 import fastifyCookie from "@fastify/cookie";
-
+import fastifyWebsocket from "@fastify/websocket";
+import { pollResults } from "./routes/ws/poll-results";
 
 const app = fastify();
 
-app.register(fastifyCookie,{
-  secret: "polls-app-nlw", 
-  hook: 'onRequest',
+app.register(fastifyCookie, {
+  secret: "polls-app-nlw",
+  hook: "onRequest",
 });
+
+app.register(fastifyWebsocket);
 
 app.register(createPoll);
 app.register(getPoll);
 app.register(voteOnPoll);
+
+app.register(pollResults);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server rodando!");
